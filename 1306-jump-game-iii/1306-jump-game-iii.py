7f4c68e05b71174e9be41b25abf5307748947408
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2026-05-17 20:10:59 +0300

    Time: 7 ms (97.28%), Space: 26.7 MB (47.47%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -7,10 +7,8 @@ class Solution:
         visited = set()
         possible = False
         queue = deque([start])
-        queue_set = set([start])
         while queue:
             poped = queue.popleft()
-            queue_set.discard(poped)
             visited.add(poped)
             x = poped + arr[poped]
             y = poped - arr[poped]
@@ -18,16 +16,14 @@ class Solution:
                 if arr[x] == 0:
                     possible = True
                     break
-                elif x not in visited and x not in queue_set:
+                elif x not in visited:
                     queue.append(x)
-                    queue_set.add(x)
                     
             if y >= 0:
                 if arr[y] == 0:
                     possible = True
                     break
-                elif y not in visited and y not in queue_set:
+                elif y not in visited:
                     queue.append(y)
-                    queue_set.add(y)
         
         return possible 
