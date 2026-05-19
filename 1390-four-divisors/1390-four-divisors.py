b6e5a4c0e921fbaa36e44218fbfdd52af6ec5806
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2026-05-20 02:24:27 +0300

    Time: 103 ms (88.54%), Space: 20.7 MB (26.88%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.py
+++ 1390-four-divisors/1390-four-divisors.py
@@ -1,10 +1,15 @@
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+
         def divisors(x):
             temp = set()
 
             for d in range(1, int(math.sqrt(x)) + 1):
                 if x % d == 0:
+                    if len(temp) == 4:
+                        return 0
+                        
                     temp.add(d)
                     temp.add(x // d)
 
