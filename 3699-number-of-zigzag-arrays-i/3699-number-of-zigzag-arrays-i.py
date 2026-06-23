fc0aad78a5fc1c564fdf54e5c1cb7fbeff4fd0c0
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2026-06-23 16:44:12 +0300

    Time: 3357 ms (97.37%), Space: 19.8 MB (57.89%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
@@ -2,17 +2,15 @@ class Solution:
     MOD = 10**9 + 7
     def zigZagArrays(self, n: int, l: int, r: int) -> int:
         m = r - l + 1
-
-        dp_0 = [1] * m
-        dp_1 = [1] * m
-
-        for _ in range(n - 1):
-            sum_0 = list(accumulate(dp_0, initial=0))
-            sum_1 = list(accumulate(dp_1, initial=0))
-
-            dp_0 = [x % self.MOD for x in sum_1[:-1]]
-
-            s0_m = sum_0[-1]
-            dp_1 = [(s0_m - x) % self.MOD for x in sum_0[1:]]
-
-        return (sum(dp_0) + sum(dp_1)) % self.MOD
+        dp = [0] + [1] * m
+        
+        for i in range(2, n + 1):
+            pref = list(accumulate(dp))
+            
+            if i % 2 == 0:
+                dp = [0] + pref[:-1]
+            else:
+                total = pref[-1]
+                dp = [0] + [(total - x) % self.MOD for x in pref[1:]]
+        
+        return (sum(dp) % self.MOD * 2) % self.MOD
