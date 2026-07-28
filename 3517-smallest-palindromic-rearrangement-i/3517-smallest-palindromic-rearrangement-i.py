10f2d8dc58de79c52b2db895aefb3588416b3bb7
Nate <167537997+nateyuel@users.noreply.github.com>
2026-07-28 18:25:49 +0300

    Time: 171 ms (76.5%), Space: 20.8 MB (79.72%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,6 +1,5 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
-        n = len(s)
         counter = Counter(s)
         elements = sorted(list(counter.keys()))
 
@@ -8,7 +7,7 @@ class Solution:
         odd_occur = ""
 
         for ch in elements:
-            if n % 2 == 1:
+            if len(s) % 2 == 1:
                 if counter[ch] % 2 == 1:
                     odd_occur = ch
             conc += (ch * (counter[ch] // 2))
