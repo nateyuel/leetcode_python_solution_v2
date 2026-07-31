a96f85c25673be178bfbd375dd16373c89ae6a09
Nate <167537997+nateyuel@users.noreply.github.com>
2026-07-31 11:32:22 +0300

    Time: 90 ms (90.97%), Space: 20.2 MB (23.08%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -4,18 +4,18 @@ class Solution:
         unique_letters = sorted(counter, reverse=True, key=counter.get)
 
         min_res = 0
-        r = 0
-        c = 1
+        phase = 0
+        count = 1
 
         for let in unique_letters:
-            if c == 1:
-                r += 1
+            if count == 1:
+                phase += 1
 
-            min_res += counter[let] * r
+            min_res += counter[let] * phase
 
-            if c == 8:
-                c = 1
+            if count == 8:
+                count = 1
             else:
-                c += 1
+                count += 1
         
         return min_res
