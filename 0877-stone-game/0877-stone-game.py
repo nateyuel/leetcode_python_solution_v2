0caeccec1b4723c4ba977f15b620d59f80ddc23d
Nate <167537997+nateyuel@users.noreply.github.com>
2026-08-02 15:50:41 +0300

    Time: 385 ms (21.02%), Space: 139.9 MB (5.7%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,17 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+
+        @lru_cache(None)
+        def dp(l, r):
+            if l > r:
+                return 0
+            
+            turn = (r - l - n) % 2
+
+            if turn == 1:
+                return max(piles[l] + dp(l+1, r), piles[r] + dp(l, r-1))
+            else:
+                return max(dp(l+1, r) - piles[l], dp(l, r-1) - piles[r])
+
+        return dp(0, n-1) > 0
