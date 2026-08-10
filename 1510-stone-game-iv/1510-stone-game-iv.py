225d009b47c390b101f6b2d7125a11f3dce87608
Nate <167537997+nateyuel@users.noreply.github.com>
2026-08-10 14:40:43 +0300

    Time: 422 ms (81.21%), Space: 19.8 MB (96.67%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -3,9 +3,13 @@ class Solution:
         dp = [False] * (n + 1)
 
         for i in range(1, n + 1):
+
             for j in range(1, int(i ** 0.5) + 1):
+
                 if not dp[i - j * j]:
+
                     dp[i] = True
+                    
                     break
 
         return dp[n]
