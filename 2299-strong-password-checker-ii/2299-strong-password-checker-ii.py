43256d697b7cdee7b45cf3964380dc3c86f3428e
Nate <167537997+nateyuel@users.noreply.github.com>
2026-08-23 12:19:04 +0300

    Time: 0 ms (100%), Space: 19.4 MB (44.68%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def strongPasswordCheckerII(self, password: str) -> bool:
         n = len(password)
         sc_dict = {char: True for char in "!@#$%^&*()-+"}
-        at_least_8 = True if n >= 8 else False
+        at_least_8_ch = True if n >= 8 else False
         one_lc = False
         one_uc = False
         one_dg = False
@@ -24,6 +24,6 @@ class Solution:
             if i > 0 and ch == password[i-1]:
                 adj_smc = False
         
-        res = at_least_8 and one_lc and one_uc and one_dg and one_sc and adj_smc
+        res = at_least_8_ch and one_lc and one_uc and one_dg and one_sc and adj_smc
 
         return res 
